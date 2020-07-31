686a51075837a97c557af19c307ae2c8712ba5e2
Draudastic <eduard.reger@gmail.com>
2020-07-31 07:31:53 +0200

    🐛 Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
+
 cp README.md document-handler-unity/Assets/DocumentHandler
 git subtree push --prefix document-handler-unity/Assets/DocumentHandler origin upm
-rm document-handler-unity/Assets/DocumentHandler/README.md
